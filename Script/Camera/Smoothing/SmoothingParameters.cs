06cafd4d5d969ae0862c303707ed258a4c6279c5
mtmfr <mmathis1806@gmail.com>
2026-04-17 17:25:35 +0200

    Setup guide
    Added setup guide as a scene

--- Script/Camera/Smoothing/SmoothingParameters.cs
+++ Script/Camera/Smoothing/SmoothingParameters.cs
@@ -1,27 +1,29 @@
-using CameraController;
 using System;
 using UnityEngine;
 
-[Serializable]
-public struct SmoothingParameters
+namespace CameraController
 {
-    [Tooltip("How long the smoothing last. 0 = no smoothing")]
-    [SerializeField, Min(0)] private float smoothTime;
-    [Tooltip("The easing applied to the smoothing.")]
-    [SerializeField] private EaseType easeType;
-
-    /// <summary>
-    /// Duration of the smoothing.
-    /// </summary>
-    public readonly float smoothingTime => smoothTime;
-    /// <summary>
-    /// The easing applied to the smoothing.
-    /// </summary>
-    public readonly EaseType EasingType => easeType;
-
-    public SmoothingParameters(float smoothingTime, EaseType easeType)
+    [Serializable]
+    public struct SmoothingParameters
     {
-        smoothTime = smoothingTime;
-        this.easeType = easeType;
+        [Tooltip("How long the smoothing last. 0 = no smoothing")]
+        [SerializeField, Min(0)] private float smoothTime;
+        [Tooltip("The easing applied to the smoothing.")]
+        [SerializeField] private EaseType easeType;
+
+        /// <summary>
+        /// Duration of the smoothing.
+        /// </summary>
+        public readonly float smoothingTime => smoothTime;
+        /// <summary>
+        /// The easing applied to the smoothing.
+        /// </summary>
+        public readonly EaseType EasingType => easeType;
+
+        public SmoothingParameters(float smoothingTime, EaseType easeType)
+        {
+            smoothTime = smoothingTime;
+            this.easeType = easeType;
+        }
     }
 }
